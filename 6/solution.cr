adeee6df80a4a6be8d384c85178bcf2580b0648b
David Schovanec <schovanec@schovi.cz>
2016-12-07 21:13:46 +0100

    Day 6

---
+++ 6/solution.cr
@@ -0,0 +1,19 @@
+input = File.read(File.join(__DIR__, "input"))
+
+signal = input.split("\n").map(&.chars)
+
+signal.first.size.times do |i|
+  memo = {} of Char => Int32
+
+  signal.each do |chars|
+    char = chars[i] 
+    memo[char] ||= 0
+    memo[char] += 1 
+  end
+
+  char, _ = memo.max_by do |char, val| val end
+
+  print char
+end
+
+print "\n"
